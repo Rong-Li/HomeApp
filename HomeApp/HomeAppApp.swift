9bc7b806f242d0c369915b2fee14d7ceaf985080
Rong-Li <lirong619@gmail.com>
2026-01-30 18:12:37 -0500

    Initial Commit

---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by Rong Li on 2026-01-30.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
